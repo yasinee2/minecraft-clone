f3a8415418af4ae3df81e508cb75bf874eed4c52
Filip Main <theelfilippo@gmail.com>
2026-01-21 00:19:32 +0100

    attempted to vibecode an FPSCamera class and debugged

--- src/main/java/com/minecraftclone/App.java
+++ src/main/java/com/minecraftclone/App.java
@@ -10,10 +10,19 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         var cam = new GLKamera();
+        var FPSCamera = new FPSCamera(cam);
         var texture = new GLTextur("C:/Users/theel/Desktop/Meine Sachen/Bilder/Privat/aihoshino.png");
         new GLLicht();
         new GLBoden(texture);
         new GLHimmel(texture);
         var keys = new GLTastatur();
+        try {
+            new CameraDebug();
+        } catch (Exception e) {}
+        while (true) {
+            try {
+                Thread.sleep(16); // ~60 FPS
+            } catch (InterruptedException e) {}
+        }
     }
 }
