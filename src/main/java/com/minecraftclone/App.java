33dbaa5eb1d59c7a9748aa0910375bd55d7dc634
Mats Oeynhausen <m.oeynhausen@icloud.com>
2026-01-23 17:14:04 +0100

    jme3 test

--- src/main/java/com/minecraftclone/App.java
+++ src/main/java/com/minecraftclone/App.java
@@ -1,26 +1,28 @@
 package com.minecraftclone;
 
-import GLOOP.*;
-import com.minecraftclone.entity.Player;
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 
-public class App {
-
-    static String textureGrass = "lib/textures/grass.png";
-    static String textureSky = "lib/textures/sky.jpg";
-    static String textureMarble = "lib/textures/marble.jpg";
+/** Sample 1 - how to get started with the most simple JME 3 application.
+ * Display a blue 3D cube and view from all sides by
+ * moving the mouse and pressing the WASD keys. */
+public class App extends SimpleApplication {
 
     public static void main(String[] args) {
-        Player player = new Player(); //Player creates cam
+        App app = new App();
+        app.start(); // start the game
+    }
 
-        new GLLicht();
-        new GLBoden(textureGrass);
-        new GLHimmel(textureSky);
-
-        while (true) {
-            player.movement();
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {}
-        }
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b); // create cube geometry from the shape
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md"); // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue); // set color of material to blue
+        geom.setMaterial(mat); // set the cube's material
+        rootNode.attachChild(geom); // make the cube appear in the scene
     }
 }
